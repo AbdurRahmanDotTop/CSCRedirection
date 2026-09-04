27171e87493936122f9e3a8438cf0235ac3aa8c7
AbdurRahmanDotTop <abdurrahmandottopofficial@gmail.com>
2026-09-04 08:46:33 +0530

    Configure redirects for Pages

--- index.js
+++
@@ -1,8 +0,0 @@
-export default {
-  async fetch(request, env, ctx) {
-    const url = new URL(request.url);
-    const targetDomain = "https://csc.techilyflycsc.workers.dev";
-    const targetUrl = new URL(url.pathname + url.search, targetDomain);
-    return Response.redirect(targetUrl.toString(), 301);
-  },
-};
